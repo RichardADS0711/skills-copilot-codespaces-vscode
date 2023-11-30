d6d1670c6bf597cb677bad93c215547931006503
Richard Durán Soto <104548778+RichardADS0711@users.noreply.github.com>
2023-11-30 04:17:12 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.value;
+    var skillsMemberText = skillsMember.options[skillsMember.selectedIndex].text;
+    var skillsMemberTextValue = skillsMemberText + " " + skillsMemberValue;
+    skills.value = skillsMemberTextValue;
+}
